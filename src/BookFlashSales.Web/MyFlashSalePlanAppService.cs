f38d017df275c5ea068390aeda99f99af75d5b5f
gdlcf88 <gdlcf88@outlook.com>
2022-08-07 22:09:50 +0800

    Override services for stress tests

---
+++ src/BookFlashSales.Web/MyFlashSalePlanAppService.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Threading.Tasks;
+using EasyAbp.EShop.Plugins.FlashSales.FlashSalePlans;
+using EasyAbp.EShop.Plugins.FlashSales.FlashSaleResults;
+using EasyAbp.EShop.Products.DaprActorsInventory;
+using EasyAbp.Eshop.Products.Products;
+using EasyAbp.EShop.Products.Products;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Options;
+using Volo.Abp.Caching;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.DistributedLocking;
+using Volo.Abp.EventBus.Distributed;
+
+namespace BookFlashSales.Web;
+
+[Dependency(ReplaceServices = true)]
+[ExposeServices(typeof(MyFlashSalePlanAppService), typeof(FlashSalePlanAppService), typeof(IFlashSalePlanAppService))]
+public class MyFlashSalePlanAppService : FlashSalePlanAppService
+{
+    public MyFlashSalePlanAppService(IFlashSalePlanRepository flashSalePlanRepository,
+        IProductAppService productAppService, IDistributedCache<FlashSalePlanPreOrderCacheItem> tokenDistributedCache,
+        IDistributedCache<FlashSalePlanCacheItem, Guid> planDistributedCache, IDistributedEventBus distributedEventBus,
+        IFlashSaleResultRepository flashSaleResultRepository, IAbpDistributedLock distributedLock,
+        IFlashSalePlanHasher flashSalePlanHasher, IFlashSaleInventoryManager flashSaleInventoryManager,
+        IDistributedCache distributedCache, IOptionsMonitor<FlashSalesOptions> optionsMonitor) : base(
+        flashSalePlanRepository, productAppService, tokenDistributedCache, planDistributedCache, distributedEventBus,
+        flashSaleResultRepository, distributedLock, flashSalePlanHasher, flashSaleInventoryManager, distributedCache,
+        optionsMonitor)
+    {
+    }
+
+    protected override async Task<FlashSalePlanPreOrderCacheItem> GetPreOrderCacheAsync(Guid planId)
+    {
+        return await base.GetPreOrderCacheAsync(planId) ?? new FlashSalePlanPreOrderCacheItem
+        {
+            TenantId = null,
+            HashToken = "my-token",
+            PlanId = planId,
+            ProductId = Guid.Parse("3a0580c5-cdaa-db74-3733-4d4d72354773"),
+            ProductSkuId = Guid.Parse("3a0580c5-d0a2-f7cc-ee6c-313d59b4b61b"),
+            InventoryProviderName = DaprActorsProductInventoryProvider.DaprActorsProductInventoryProviderName
+        };  // for stress tests
+    }
+}
---
+++ src/BookFlashSales.Web/MyFlashSalePlanHasher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using EasyAbp.EShop.Plugins.FlashSales.FlashSalePlans;
+using Volo.Abp.DependencyInjection;
+
+namespace BookFlashSales.Web;
+
+[Dependency(ReplaceServices = true)]
+[ExposeServices(typeof(MyFlashSalePlanHasher), typeof(FlashSalePlanHasher), typeof(IFlashSalePlanHasher))]
+public class MyFlashSalePlanHasher : FlashSalePlanHasher
+{
+    public override Task<string> HashAsync(DateTime? planLastModificationTime, DateTime? productLastModificationTime,
+        DateTime? productSkuLastModificationTime)
+    {
+        return Task.FromResult("my-token");
+    }
+}
